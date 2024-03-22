2840d6e1365f4edc607866d5c973f379ece22123
Leo <leo.hilsheimer@gmail.com>
2024-03-22 09:25:58 +0100

    reduce font download size by removing customFetch function and unused styles

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig, presetUno, presetWind, presetIcons } from "unocss";
 import presetWebFonts from "@unocss/preset-web-fonts";
-import axios from "axios";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetWind() /* tailwind compatible */,
     presetWebFonts({
-      customFetch: (url: string) => axios.get(url).then((it) => it.data),
       provider: "google",
       fonts: {
         serif: {
           name: "Kaisei HarunoUmi",
-          weights: [400, 500, 700],
-        },
-        sans: {
-          name: "Lato",
-          weights: [400, 500, 700],
+          weights: [400],
+          italic: true,
         },
       },
     }),
