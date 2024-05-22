988c7acf0a9608e489b2b090777bf3fec75bacc5
Leo <leo.hilsheimer@gmail.com>
2024-05-22 21:31:49 +0200

    fix 404 page

---
+++ src/app/(app)/[locale]/not-found.tsx
@@ -0,0 +1,9 @@
+import Gutter from '@/app/components/Gutter'
+
+export default function Custom404() {
+  return (
+    <Gutter className="min-h-md flex items-center justify-center">
+      <h1>404 - Page Not Found</h1>
+    </Gutter>
+  )
+}
