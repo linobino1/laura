e046f46e2029588c71afcfb44cdde2f2f6479f04
Leo <leo.hilsheimer@gmail.com>
2024-05-22 11:11:31 +0200

    use correct locale

--- src/app/(app)/[locale]/[page]/page.tsx
+++ src/app/(app)/[locale]/[page]/page.tsx
@@ -1,22 +1,31 @@
-import Blocks from '@/app/components/blocks/Blocks'
 import { createPayload } from '@/app/util/createPayload'
 import PageComponent from '@/app/components/Page'
+import { Locale, validateLocale } from '@/app/util/validateLocale'
+import { notFound } from 'next/navigation'
 
-export default async function Page({ params }: { params: { page: string } }) {
+export default async function Page({
+  params: { locale, page: slug },
+}: {
+  params: { locale: string; page: string }
+}) {
+  if (!validateLocale(locale)) {
+    return notFound()
+  }
   const payload = await createPayload()
   const page = (
     await payload.find({
       collection: 'pages',
       where: {
         slug: {
-          equals: params.page,
+          equals: slug,
         },
       },
+      locale: locale as Locale,
     })
   ).docs[0]
 
   if (!page) {
-    return <div>Page not found</div>
+    return notFound()
   }
 
   return <PageComponent layout={page.layout} />
--- src/app/(app)/[locale]/works/[slug]/page.tsx
+++ src/app/(app)/[locale]/works/[slug]/page.tsx
@@ -1,17 +1,26 @@
 import { createPayload } from '@/app/util/createPayload'
 import { notFound } from 'next/navigation'
 import PageComponent from '@/app/components/Page'
+import { Locale, validateLocale } from '@/app/util/validateLocale'
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({
+  params: { locale, slug },
+}: {
+  params: { locale: string; slug: string }
+}) {
+  if (!validateLocale(locale)) {
+    return notFound()
+  }
   const payload = await createPayload()
   const work = (
     await payload.find({
       collection: 'works',
       where: {
         slug: {
-          equals: params.slug,
+          equals: slug,
         },
       },
+      locale: locale as Locale,
     })
   ).docs[0]
 
