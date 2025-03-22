7dbea805f3c1b7504ae4efcdd63436682091eb83
Leo <leo.hilsheimer@gmail.com>
2025-03-22 19:24:36 +0100

    fix: back button reverts locale choice

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -2,13 +2,16 @@
 import Link from 'next/link'
 import Gutter from './Gutter'
 import { usePathname } from 'next/navigation'
+import { useLocale } from '../providers/LocaleProvider'
 
 const BackButton = () => {
   const pathname = usePathname()
+  const { locale } = useLocale()
+
   return !['/', '/en', '/ee'].includes(pathname) ? (
     <Gutter size="lg">
       <div className="lg:absolute">
-        <Link href="/">
+        <Link href={`/${locale ?? ''}`}>
           <div className="i-teenyicons:arrow-left-solid text-2xl" />
         </Link>
       </div>
